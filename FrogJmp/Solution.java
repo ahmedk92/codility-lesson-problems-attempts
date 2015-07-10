b34701506faa4a9cea2a0353c2dfa449c58717cb
ahmedk92 <ahmedkhalaf.92@gmail.com>
2015-07-10 19:05:34 +0000

    FrogJmp

---
+++ FrogJmp/Solution.java
@@ -0,0 +1,12 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can use System.out.println for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+public class Solution {
+    public int solution(int X, int Y, int D) {
+        // write your code in Java SE 8
+        return (int) Math.ceil(((double) Y - X) / D);
+    }
+}
